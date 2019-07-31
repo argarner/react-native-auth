3e25ae702220c05bb27b826bee2c17a9e7aa4c6a
Andrew Garner <agarner84@gmail.com>
2019-07-31 19:53:38 +1000

    add firebase imports & LoginForm comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from 'react-native-dotenv';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
-const App = () => {
-  return (
-    <SafeAreaView>
-      <Header headerText="App!" />
-    </SafeAreaView>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: apiKey,
+      authDomain: authDomain,
+      databaseURL: databaseURL,
+      projectId: projectId,
+      storageBucket: storageBucket,
+      messagingSenderId: messagingSenderId,
+      appId: appId,
+    });
+  }
+  render() {
+    return (
+      <SafeAreaView>
+        <Header headerText="Auth" />
+        <LoginForm />
+      </SafeAreaView>
+    );
+  }
+}
 
 export default App;
